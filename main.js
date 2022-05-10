1380d3e7332804123f196e23f1c01ba1606f9501
Tiago Pereira <71046422+tiagocpereira@users.noreply.github.com>
2022-05-10 09:53:05 -0300

    commit menu main

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 /*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function () {
+      nav.classList.toggle('show')
+    })
+  }
